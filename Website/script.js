a189505e26789adb713bbb1da188374faf2f6014
Tania <hrundatania@gmail.com>
2023-05-17 23:06:35 +0300

    Create a registration form

--- Website/script.js
+++ Website/script.js
@@ -3,5 +3,5 @@ const button = document.querySelector(".btn_nav");
 // Додати обробник події "click" до кнопки
 button.addEventListener("click", function () {
   // Перейти на іншу сторінку
-  window.location.href = "about.html";
+  window.location.href = "sign.html";
 });
