9529fd579dfc04d11b83842c7a38d27aea999d14
Tania <hrundatania@gmail.com>
2023-05-17 21:15:16 +0300

    Create word document

--- Website/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.querySelector(".btn_nav");
-
-// Додати обробник події "click" до кнопки
-button.addEventListener("click", function () {
-  // Перейти на іншу сторінку
-  window.location.href = "about.html";
-});
