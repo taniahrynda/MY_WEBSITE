48180e4fcad1cd49bb86cb96e702642661a1b57e
Tania <hrundatania@gmail.com>
2023-05-18 08:38:12 +0300

    Change file

--- Website/script.js
+++ Website/script.js
@@ -5,3 +5,25 @@ button.addEventListener("click", function () {
   // Перейти на іншу сторінку
   window.location.href = "sign.html";
 });
+
+const form = document.getElementById("registrationForm");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault(); // Зупинити стандартну поведінку форми
+
+  const formData = new FormData(form);
+
+  fetch("/register", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data); // Вивести отримані дані в консоль
+      // Додаткові дії після обробки форми
+      // ...
+    })
+    .catch((error) => {
+      console.error("Помилка:", error);
+    });
+});
--- Website/server.js
+++ Website/server.js
@@ -4,6 +4,9 @@ const mysql = require("mysql");
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({ extended: true })); // Доданий посередник для обробки даних форми
+
+// Налаштування підключення до бази даних
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
@@ -11,29 +14,48 @@ const connection = mysql.createConnection({
   database: "website",
 });
 
-function getDataFromDatabase() {
-  return new Promise((resolve, reject) => {
-    connection.query("SELECT * FROM CUSTOMER", (err, results) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-      resolve(results);
-    });
-  });
-}
-
-app.get("/", (req, res) => {
-  const data = getDataFromDatabase();
-  data
-    .then((results) => {
-      res.send(results);
-    })
-    .catch((err) => {
-      res.send("Error fetching data from database");
-    });
+// Підключення до бази даних
+connection.connect((err) => {
+  if (err) {
+    console.error("Помилка підключення до бази даних:", err);
+  } else {
+    console.log("Підключено до бази даних");
+  }
 });
 
+// Маршрут для обробки POST-запиту з форми реєстрації
+app.use(express.static(__dirname + "/Website"));
+app.post("/register", (req, res) => {
+  const {
+    name,
+    surname,
+    email,
+    password,
+    gender,
+    dateOfBirth,
+    city,
+    phoneNumber,
+  } = req.body;
+
+  // Збереження даних в базі даних
+  const query = `INSERT INTO users (FIRSTNAME, LASTNAME, EMAIL, PASS_WORD, GENDER, DATE_BIRTH, CITY, PHONE_NUMBERS)
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+  connection.query(
+    query,
+    [name, surname, email, password, gender, dateOfBirth, city, phoneNumber],
+    (err, result) => {
+      if (err) {
+        console.error("Помилка при збереженні даних:", err);
+        res.status(500).send("Виникла помилка при збереженні даних");
+      } else {
+        console.log("Дані успішно збережено в базі даних");
+        res.status(200).send("Дані успішно збережено");
+      }
+    }
+  );
+});
+
+// Запуск сервера
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Сервер запущено на порту ${port}`);
 });
