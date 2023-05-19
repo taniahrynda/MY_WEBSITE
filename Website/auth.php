4e3e274e5ceb651ba3ebbdf331538ac82e5c27fe
Tania <hrundatania@gmail.com>
2023-05-20 00:11:17 +0300

    Setting log in on the site

---
+++ Website/auth.php
@@ -0,0 +1,16 @@
+<?php
+$email = filter_var(trim($_POST['email']), FILTER_SANITIZE_STRING);
+$password = filter_var(trim($_POST['password']), FILTER_SANITIZE_STRING);
+
+$mysql = new mysqli('localhost','root','','website');
+$result = $mysql->query("SELECT * FROM `customer` WHERE `EMAIL`= '$email' AND `PASS_WORD` = '$password'");
+$user = $result->fetch_assoc();
+if (count($user) == 0){
+  echo "Такий користувач не знайдений";
+  exit();
+}
+setcookie('user',$user['name'],time() + 3600, "/");
+$mysql->close();
+
+header('Location:/book.html')
+?>
