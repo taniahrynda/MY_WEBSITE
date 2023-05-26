6fda82ed268b9b2f14cd47eaaa67540ef03ba325
Tania <hrundatania@gmail.com>
2023-05-26 21:46:11 +0300

    Button add delete, update, add

--- Website/get_data_employee.php
+++ Website/get_data_employee.php
@@ -49,9 +49,76 @@
             <td><?php echo $row['STREET']; ?></td>
            
           </tr>
+      
         <?php endforeach; ?>
 
       </table>
+      <button id="addButton">Додати</button>
+
+<div id="addForm" style="display: none;">
+  <h3>Додавання нового працівника</h3>
+  <form action="add_employee.php" method="POST">
+    <input class="sign_text" type="text" name="ID_emp" placeholder="Введіть ID" required><br>
+    <input class="sign_text" type="text" name="firstname" placeholder="Введіть ім’я" required><br>
+    <input class="sign_text" type="text" name="lastname" placeholder="Введіть прізвище" required><br>
+    <input class="sign_text" type="text" name="position_emp" placeholder="Введіть посаду" required><br>
+    <input class="sign_text" type="text" name="office" placeholder="Введіть ID магазину" required><br>
+    <input class="sign_text" type="text" name="rate_emp" placeholder="Введіть зарплату" required><br>
+    <input type="submit" value="Підтвердити додавання працівника" />
+  </form>
+</div>
+<button id="deleteButton">Видалити</button>
+
+<div id="deleteForm" style="display: none;">
+  <h3>Видалення працівника</h3>
+  <form action="delete_emp.php" method="POST">
+    <input class="sign_text" type="text" name="emp_id" placeholder="Введіть ID працівника, якого хочете видалити" required><br>
+    <input type="submit" value="Підтвердити видалення" />
+  </form>
+</div>
+
+<script>
+  var deleteButton = document.getElementById("deleteButton");
+  var deleteForm = document.getElementById("deleteForm");
+
+  deleteButton.addEventListener("click", function() {
+    deleteForm.style.display = "block";
+  });
+</script>
+
+
+
+<button id="updateButton">Оновити</button>
+
+<div id="updateForm" style="display: none;">
+  <h3>Оновлення зарплати працівника</h3>
+  <form action="update_emp.php" method="POST">
+    <input class="sign_text" type="text" name="product_id" placeholder="Введіть ID працівника" required><br>
+    <input class="sign_text" type="text" name="price_book" placeholder="Введіть нову зарплату працівника" required><br>
+    <input type="submit" value="Підтвердити оновлення" />
+  </form>
+</div>
+
+<script>
+  var addButton = document.getElementById("addButton");
+  var addForm = document.getElementById("addForm");
+
+  addButton.addEventListener("click", function() {
+    addForm.style.display = "block";
+  });
+</script>
+
+
+
+<script>
+  var updateButton = document.getElementById("updateButton");
+  var updateForm = document.getElementById("updateForm");
+
+  updateButton.addEventListener("click", function() {
+    updateForm.style.display = "block";
+  });
+</script>
+
     <?php endif; ?>
   </body>
 </html>
